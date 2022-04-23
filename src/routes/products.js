8b337d23d9120c2d06dbd75a6954b310201e1324
Leblois <isaacleblois@gmail.com>
2022-04-23 04:46:27 -0500

    Primer commit

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,41 +1,28 @@
 //Packages
 const { Router } = require('express')
-const Container = require('../../fileSystem')
+const Container = require('../fileSystem')
 const router = new Router()
 const database = new Container("data")
 
 //ROUTES
-//Get all
+//Panel de control
 router.get('/', async (req, res) => {
-    try {
-        const allProducts = await database.getAll()
-        res.json(allProducts)
-    } catch(err) {
-        console.log(err)
-    }
+    res.render('control')
 })
 
-//Get by ID
-router.get('/:id', async (req, res) => {
+router.get('/productos', async (req, res) => {
     try {
-        const id = req.params.id
-        const product = await database.getById(id)
-
-        if (!product) {
-            res.send({error: 'producto no encontrado.'})
-        } else {
-            res.json(product)
-        }
+        const allProducts = await database.getAll()
+        res.render('productos', {allProducts})
     } catch(err) {
         console.log(err)
     }
 })
 
 //Add new product
-router.post('/', async (req, res) => {
+router.post('/productos', async (req, res) => {
     try {
         const allProducts = await database.getAll()
-        const noImage = 'https://cdn1.iconfinder.com/data/icons/carbon-design-system-vol-6/32/no-image-256.png'
         let lastID = 0
     
         if(allProducts.length) {
@@ -46,48 +33,11 @@ router.post('/', async (req, res) => {
             id: lastID + 1,
             title: req.body.title ? req.body.title : 'No Title',
             price: req.body.price ? req.body.price : 0,
-            thumbnail: req.body.thumbnail ? req.body.thumbnail : noImage
+            thumbnail: req.body.thumbnail ? req.body.thumbnail : "No Image"
         }
 
         await database.add(newProduct)
-        res.json(newProduct)
-    } catch(err) {
-        console.log(err)
-    }
-})
-
-//Edit product
-router.put('/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        const product = await database.getById(id)
-
-        const editedProduct = {
-            id: product.id,
-            title: req.body.title ? req.body.title : product.title,
-            price: req.body.price ? req.body.price : product.price,
-            thumbnail: req.body.thumbnail ? req.body.thumbnail : product.thumbnail
-        }
-
-        await database.editById(editedProduct)
-
-        res.json(editedProduct)
-    } catch(err) {
-        console.log(err)
-    }
-})
-
-//Delete product
-router.delete('/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        const resp = await database.deleteById(id)
-
-        if(!resp) {
-            res.send(`El producto con ID ${id} no existe.`)
-        } else {
-            res.send(`El producto con ID ${id} ha sido removido.`)
-        }
+        res.redirect('/productos')
     } catch(err) {
         console.log(err)
     }
--- src/servidor.js
+++ src/servidor.js
@@ -2,18 +2,24 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const exphbs = require('express-handlebars').engine
+const productsRouter = require('./routes/products')
 
+//Settings
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-//Routes require
-const mainRouter = require('./routes/main')
-const productsRouter = require('./routes/products')
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')    
 
 //Routes use
-app.use('/', mainRouter)
-app.use('/api/productos', productsRouter)
+app.use('/', productsRouter)
 
 //Server configuration
 const PORT = 8080
